5f2998956ab0b466a8562a13997603f173f0d685
Baylee Duncan <baylee.duncan@protonmail.com>
2022-08-31 16:33:54 -0500

    updated file

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -3,8 +3,9 @@ def safe_print_division(a, b):
     try:
         result = a / b
         print("Inside result: {}".format(result))
-    except b is 0:
-        result = "None"
-        print("Inside result: {}".format(result))
+    except:
+        if b == 0:
+            result = "None"
+            print("Inside result: {}".format(result))
     finally:
         print("{} / {} = {}".format(a, b, result))
