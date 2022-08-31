51036d4b083d775e886ef500145f833169fcddc3
Baylee Duncan <baylee.duncan@protonmail.com>
2022-08-31 16:07:31 -0500

    added file

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    arg = len(sys.argv) - 1
+    if arg == 0:
+        print("0 arguments.")
+    elif arg == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(arg))
+    for x in range(arg):
+        print("{}: {}".format(x + 1, sys.argv[x + 1]))
