3a01230859b5627a601290e69c6cbc0cab4d2ed6
Baylee Duncan <baylee.duncan@protonmail.com>
2022-08-31 18:50:21 -0500

    updated file

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -6,4 +6,4 @@ def safe_print_division(a, b):
         result = "None"
     finally:
         print("Inside result: {}".format(result))
-        print("{} / {} = {}".format(a, b, result))
+        return result
