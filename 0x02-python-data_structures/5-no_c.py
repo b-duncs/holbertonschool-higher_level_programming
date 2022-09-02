f2a028b6d3cb413cb361a51af6269049bf2a6222
Baylee Duncan <baylee.duncan@protonmail.com>
2022-09-01 20:32:17 -0500

    updated file

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for x in range(len(my_string)):
-        x = "Cc"
-        y = ""
-        new_string = my_string.maketrans(x, y)
+        new_string = {'C': None, 'c': None}
         return my_string.translate(new_string)
