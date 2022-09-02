6e108314614ca3d9e1df7f2f00990dd620ddf23c
Baylee Duncan <baylee.duncan@protonmail.com>
2022-09-01 20:23:52 -0500

    updated file

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -2,7 +2,5 @@
 def no_c(my_string):
     for x in range(len(my_string)):
         if x != 'c' and x != 'C':
-            remove = ""
-            mod_string = remove.join(my_string)
-            return mod_string
-        
+            return "".join(x)
+
