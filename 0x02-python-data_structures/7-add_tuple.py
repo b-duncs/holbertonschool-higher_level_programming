d74238d0c4c877632242690e135c892c34026f41
Baylee Duncan <baylee.duncan@protonmail.com>
2022-09-01 21:19:58 -0500

    updated file

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -10,5 +10,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
-    result = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
+    result = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return result
