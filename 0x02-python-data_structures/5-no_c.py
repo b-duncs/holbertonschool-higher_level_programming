4beef324b75d76ae603ea10e79bb9e5b156edcfb
Baylee Duncan <baylee.duncan@protonmail.com>
2022-09-01 20:22:42 -0500

    updated file

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for x in range(len(my_string)):
-        my_string['C'] = ""
-        my_string['c'] = ""
-        return my_string
-
+        if x != 'c' and x != 'C':
+            remove = ""
+            mod_string = remove.join(my_string)
+            return mod_string
+        
