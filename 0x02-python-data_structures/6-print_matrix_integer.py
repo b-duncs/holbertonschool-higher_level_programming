74e5423e2c19592a4eb10ba23fac3abd4cc94850
Baylee Duncan <baylee.duncan@protonmail.com>
2022-09-01 21:07:42 -0500

    updated file

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        for y in x:
-            print(matrix[x][y], end=" ")
-        print()
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            print("{:d}".format(matrix[x][y]), end="")
+            while y != (len(matrix[x]) - 1):
+                print(" ", end="")
+                break
+        print("")
