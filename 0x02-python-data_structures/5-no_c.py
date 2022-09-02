288ce1b249ffd0f14bb6d27f4fa3b1e4469e6cd2
Baylee Duncan <baylee.duncan@protonmail.com>
2022-09-01 20:34:26 -0500

    updated file

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = {'C': None, 'c': None}
-    return my_string.translate(new_string)
+    remove = {'C': None, 'c': None}
+    new_string = my_string.translate(remove)
+    return new_string
