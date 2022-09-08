24b4a50e14ce1c369298555c72928ffee04d2a9a
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-09-07 22:48:28 -0500

    updated files

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a class Square that defines a square by:
+    -Private instance attribute: size
+    -Instantiation with size (no type/value verification)
+    -You are not allowed to import any module
+"""
+
+
+class Square:
+    """Class Square with Private Attribute Size"""
+    def __init__(self, size):
+        self.__size = size
