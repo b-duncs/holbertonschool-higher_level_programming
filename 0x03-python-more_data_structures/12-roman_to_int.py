25d72414245f098c9c0d5832b356fa290a93bc0e
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-09-08 14:11:00 -0500

    updated files

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -14,11 +14,9 @@ def roman_to_int(roman_string):
     }
     num = 0
     for x in range(len(roman_string)):
-        if roman_dict.get(roman_string[x], 0) == 0:
-            return 0
         if x != len(roman_string) - 1 and \
                 roman_dict[roman_string[x]] < roman_dict[roman_string[x + 1]]:
-            num += roman_dict[roman_string[x]] * -1
+            num += roman_dict[roman_string[x]]
         else:
             num += roman_dict[roman_string[x]]
     return num
