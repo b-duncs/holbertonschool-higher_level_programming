f1e1e99c23b1caf103f09d721b2f56ae0ecd46cb
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-09-07 23:52:15 -0500

    updated files

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -7,7 +7,8 @@ Write a class Square that defines a square by: (based on 1-square.py)
         with the message size must be an integer
         -if size is less than 0, raise a ValueError exception
         with the message size must be >= 0
-    -Public instance method: def area(self): that returns the current square area
+    -Public instance method: def area(self):
+        that returns the current square area
     -You are not allowed to import any module
 """
 
