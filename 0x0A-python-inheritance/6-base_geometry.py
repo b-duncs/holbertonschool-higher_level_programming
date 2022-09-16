68a6a56147e63b107cb98fbea43bf6f8617a896d
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-09-15 22:12:54 -0500

    updated files

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry (based on 5-base_geometry.py).
+    -You are not allowed to import any module
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry with Public Instance Area"""
+
+    def area(self):
+        """Raises an Exception with Message"""
+        raise Exception("area() is not implemented")
