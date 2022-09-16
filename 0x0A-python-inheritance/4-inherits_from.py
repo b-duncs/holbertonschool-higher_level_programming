cdb403002575c597d78c17dafd4eb64e89210e6b
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-09-15 22:08:22 -0500

    updated files

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Write a function that returns True
-    if the object is an instance of a class that inherited (directly or indirectly)
+    if the object is an instance of a class
+    that inherited (directly or indirectly)
     from the specified class; otherwise False.
     -You are not allowed to import any module
 """
