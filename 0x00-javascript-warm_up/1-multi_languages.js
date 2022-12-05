b9490ae52e7a28e0eee3c8d5c5d684b265d8211d
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-12-05 17:26:59 -0600

    updated files

--- 0x00-javascript-warm_up/1-multi_languages.js
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-const first_l = 'C is fun';
-const second_l = 'Python is cool';
-const third_l = 'JavaScript is amazing';
-console.log(first_l, second_l, third_l);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
